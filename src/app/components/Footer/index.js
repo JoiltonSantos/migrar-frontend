2b06d017d68bc72cc7316171006396383f669315
JoiltonSantos <joilton7santos@hotmail.com>
2024-07-03 13:31:45 -0300

    feat(components): adicionar o componente Footer

---
+++ src/app/components/Footer/index.js
@@ -0,0 +1,9 @@
+import styles from './footer.module.css'
+
+export default function Footer() {
+	return (
+		<footer className={styles.footer}>
+			&copy; 2023 Joilton. Todos os direitos reservados.
+		</footer>
+	)
+}
